30d093e5760866768276c22126a34c13c1629945
chentao <1589921770@qq.com>
2025-06-12 19:41:26 +0800

    第3.1次提交

--- src/main/java/org/example/javawebbigjob/config/WebConfig.java
+++ src/main/java/org/example/javawebbigjob/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
